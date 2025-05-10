058ea068f0491e75ddad54b45ce0f3430583cb18
Thejpal <ramannagarithejpal3717@gmail.com>
2025-05-10 12:51:39 +0530

    General authentication for each route

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from typing import Optional
 from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel, Field, model_validator
 from src.model import UserModel
 from src.services import UserService
 from uuid import UUID
 
 app = FastAPI()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl = "/auth/login")
 
 class UserRequest(BaseModel):
     name: str
@@ -27,3 +29,7 @@ class UserResponse(UserModel):
 def register_user(user_request: UserRequest, user_service: UserService = Depends()):
     user = user_service.create_user(name = user_request.name, email = user_request.email, password = user_request.password)
     return user
+
+@app.get("/auth/user/{user_id}", response_model = UserResponse)
+def get_user(user_id: UUID, token: str = Depends(oauth2_scheme)):
+    return {"user_id": user_id}
