4c268d0916caa00fb3eb5469984b43cb67bb5b2a
Thejpal <ramannagarithejpal3717@gmail.com>
2025-06-03 04:14:57 +0530

    Code to verify and create database and tables if they don't exist

--- src/db.py
+++ src/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Column, create_engine
+from sqlalchemy import String, Column, create_engine, inspect
 from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from sqlalchemy_utils import database_exists, create_database
 from src.settings import settings
@@ -7,14 +7,6 @@ from src.logger import logger
 engine = create_engine(settings.database_url)
 Base = declarative_base()
 
-def create_db():
-    if not database_exists(engine.url):
-        create_database(engine.url) # For creating postgresql database
-        Base.metadata.create_all(engine) # For creating tables
-        logger.info("Database created successfully")
-    else:
-        logger.info("Database already exists")
-
 class User(Base):
     __tablename__ = "user"
 
@@ -22,6 +14,20 @@ class User(Base):
     name = Column(String, nullable = False, unique = True)
     email = Column(String)
     password = Column(String, nullable = False)
+
+def create_db():
+    if not database_exists(engine.url):
+        create_database(engine.url) # For creating postgresql database
+        logger.info("Database created successfully")
+    else:
+        logger.info("Database already exists")
+    
+    inspector = inspect(engine)
+    if not inspector.has_table("user"):
+        Base.metadata.create_all(engine) # For creating tables
+        logger.info("User table created successfully")
+    else:
+        logger.info("User table already exists")
 
 Session = sessionmaker(engine)
 
