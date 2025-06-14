ad0b4136ca91658cfa22eef087f7426b7f7f7d44
Thejpal <ramannagarithejpal3717@gmail.com>
2025-06-14 13:09:54 +0530

    Updating logic to wait for database connection to be ready

--- src/db.py
+++ src/db.py
@@ -1,8 +1,7 @@
-import time
-
 from sqlalchemy import String, Column, create_engine, inspect
 from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.exc import OperationalError
 
 from src.settings import settings
 from src.logger import logger
@@ -19,29 +18,24 @@ class User(Base):
     password = Column(String, nullable = False)
 
 def initialize_database():
-    i = 0
-    while i<3:
-        try:
-            if not database_exists(engine.url):
-                create_database(engine.url) # For creating postgresql database
-                logger.info("Database created successfully")
-            else:
-                logger.debug("Database already exists")
-            
-            inspector = inspect(engine)
-            if not inspector.has_table("user"):
-                Base.metadata.create_all(engine) # For creating tables
-                logger.info("User table created successfully")
-            else:
-                logger.debug("User table already exists")
-            
-            logger.debug("Breaking out of the loop after successful creation")
-            break
+    try:
+        if not database_exists(engine.url):
+            create_database(engine.url) # For creating postgresql database
+            logger.info("Database created successfully")
+        else:
+            logger.debug("Database already exists")
         
-        except Exception as e:
-            i += 1
-            time.sleep(2)
-            logger.error(f"Failed to create database or table with error: {e}. Retrying times {i+1} after 2 seconds...")
+        inspector = inspect(engine)
+        if not inspector.has_table("user"):
+            Base.metadata.create_all(engine) # For creating tables
+            logger.info("User table created successfully")
+        else:
+            logger.debug("User table already exists")
+        
+        logger.debug("Breaking out of the loop after successful creation")
+        
+    except OperationalError as e:
+        logger.error(f"Database is not ready to accept connections yet. Waiting for it to get ready: {e}")
 
 Session = sessionmaker(engine)
 session = Session()
